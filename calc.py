1fc861f38ff13c0574c7bebb324db237a3794b05
tystik <rastor132@gmail.com>
2020-10-13 01:05:10 +0600

    something has changed

---
+++ calc.py
@@ -0,0 +1,16 @@
+print("Enter first number")
+a = float(input())
+print("Enter second number")
+b = float(input())
+print("What operation")
+sign = input()
+
+if sign == '+':
+    print(a+b)
+elif sign == '-':
+    print(a-b)
+elif sign == '/':
+    print(a/b)
+elif sign == '*':
+    print(a*b)
+    
